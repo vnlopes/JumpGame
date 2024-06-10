f92298ebe64bc5fb1292d453b5f41a00fb64fb99
VINICIUS LOPES DA SILVA <0200832311013>
2024-06-10 12:06:07 -0300

    add place movement

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,31 @@
 const place = document.querySelector(".place");
 
 let windowWidth = window.innerWidth;
-let j = 10; 
+let j = 10;
 
 const moveRight = () => {
-  if (j < windowWidth - 150) {
+  if (j < windowWidth - place.offsetWidth - 10) { // Ajuste para garantir que não ultrapasse a tela
     j += 30;
+  } else {
+    j = windowWidth - place.offsetWidth - 10; // Mantém dentro do limite da tela
   }
-  place.style.marginLeft = j + "px";
-  console.log(j);
-  console.log(window.innerWidth);
+  place.style.left = j + "px";
+  console.log("Right:", j);
 };
 
 const moveLeft = () => {
   if (j > 0) {
     j -= 30;
+  } else {
+    j = 0; // Mantém dentro do limite da tela
   }
-  place.style.marginLeft = j + "px";
-  console.log(j);
-  console.log(window.innerWidth);
+  place.style.left = j + "px";
+  console.log("Left:", j);
 };
+
+window.addEventListener("resize", () => {
+  windowWidth = window.innerWidth;
+});
 
 addEventListener("keydown", (event) => {
   const keyName = event.key;
